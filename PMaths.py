3602f0bbdb37501968f0575b8fee2f3211967d46
Mohamed Rashad <thegreatrashad@yahoo.com>
2015-08-03 03:12:29 +0200

    added encoder and pi generator

--- PMaths.py
+++ PMaths.py
@@ -1,20 +1,53 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+# @author Mohamed rashad
+
+import sys
+import math
+import binascii
+
 from math import *
+from sys import *
+from decimal import *
+
+##########################
+
+def calc(n):
+
+    t= Decimal(0)
+    pi = Decimal(0)
+    deno= Decimal(0)
+    k = 0
+
+    for k in range(n):
+
+        t = ((-1)**k)*(factorial(6*k))*(13591409+545140134*k)
+        deno = factorial(3*k)*(factorial(k)**3)*(640320**(3*k))
+        pi += Decimal(t)/Decimal(deno)  
+                                 
+    pi = pi * Decimal(12)/Decimal(640320**(1.5))
+    pi = 1/pi
+
+    return pi
+
+
+##########################
 
 
 while True:
 
-    print "\n\n   ~~~~~~~~~~~~~~~~~~Welcome to PMaths Tool~~~~~~~~~~~~~~~~~~~"
 
-    print "\n Would you like to do..?\n\t1-check even or odd\n\t2-check prime number or not\n\t3-Check Divisbility\n\t4-Check golden ratio"
+
+    print "\n\n   ~~~~~~~~~~~~~~~~~~ PMaths Tool (Ultimate Python Maths App) ~~~~~~~~~~~~~~~~~~~"
+
+    print "\n Would you like to do..?\n\t1-check even or odd\n\t2-check prime number or not\n\t3-Check Divisbility\n\t4-Check golden ratio\n\t5-Compute Factorial\n\t6-Compute Fibonacci series\n\t7-Compute Pi (Limit 10 million)\n\t8-Encode Text \n\t9-Decode To text"
 
     print "\n Choose your Option:"
 
     Input = raw_input("> ")
 
-                               #######
+##########################
 
     if Input == "1":
 
@@ -27,7 +60,7 @@ while True:
         else:
             print "Your number is even"
 
-                                #######
+##########################
 
 
     elif Input == "2":
@@ -50,7 +83,8 @@ while True:
         else:
             print "This is not a Prime Number"
 
-                               #######
+##########################
+
     elif Input == "3":
 
         print "\n Enter Desired Number Here"
@@ -70,7 +104,7 @@ while True:
             print "\n Your input is divisible by %r" %Input4
 
 
-                               #######
+##########################
 
 
     elif Input == "4":
@@ -101,21 +135,28 @@ while True:
             T = n/m
             Q = float("{0:.1f}".format(T))
      
-            if Q == 1.6:
+            if Q > 1.6 and Q < 1.7:
 
-                print "\n Golden Ratio exists between these 2 digits"
+                print "\n Golden Ratio exists between these 2 digits (approximately)"
+
             else:
+
                 print R
                 print T
                 print "\n Golden Ratio  doesn't exist between these 2 digits"   
 
-  elif Input == "5":
+    
+
+##########################
+
+    elif Input == "5":
 
         print "\n insert a number (integer only)"
         n = int(raw_input("> "))
 
         print str(math.factorial(n))
 
+##########################
 
     elif Input == "6":
 
@@ -129,11 +170,58 @@ while True:
             a.append(a[n]+a[n-1])
             print a[-1]
 
+    elif Input == "7" :
+        print "\n How many digits of pi to generate ? (limit 10 million)"
+        n = int(raw_input("> "))
+        print "\n"
+        print "\n"
+
+        print "Pi = " + str(calc(n))
+
+##########################
+
+
+    elif Input == "8" :
+        print "\n Choose conversion type\n\t1-Binary\n\t2-Octal\n\t3-Hex "
+ 
+        n = raw_input("> ")
+        if n == "1" :
+            print "\n Enter Text"
+            mm = raw_input("> ")
+            print  str(bin(int(binascii.hexlify(mm), 16))).replace("0b" , "")
+
+        elif n == "3" :
+            print "\n Enter Text"
+            mm = raw_input("> ")
+            print  str(":".join("{:02x}".format(ord(c)) for c in mm))
+
+        elif n == "2" :
+            print "\n Enter number"
+            mm = int(raw_input("> "))
+            print  str(oct(mm))
 
 
 
+##########################
 
-        
+
+    elif Input == "9" :
+        print "\n Choose conversion type\n\t1-Binary\n\t2-Octal\n\t3-Hex "
+ 
+        n = raw_input("> ")
+        if n == "1" :
+            print "\n Enter Binary"
+
+
+        elif n == "3" :
+            print "\n Enter Hex"
+
+
+        elif n == "2" :
+            print "\n Enter Octal"
+
+
+##########################
 
 
 
